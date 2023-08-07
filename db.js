b2e7809188397ca67ec7619e2b990c47adc8fdbf
Terra SARR <samba.co93@gmail.com>
2023-08-07 18:36:08 +0000

    mongoose files

---
+++ db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const User = require("./User");
+
+mongoose.connect("mongodb://localhost/mongodb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Erreur de connexion :"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+module.exports = mongoose;
